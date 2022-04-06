935ad6b5264c8147792c8c0f36794a9a440d4e02
Linonon <an493454270@gmail.com>
2022-04-06 09:45:18 +0800

    Push without message

--- timed-task/sh/autocommit.sh
+++ timed-task/sh/autocommit.sh
@@ -6,7 +6,7 @@ function cdgpacp() {
     pwd
 	git pull && git add .
 	if [[ $2 != "" ]]; then
-		git commit -m $2
+		git commit -m "定時提交:"$(date '+%Y-%m-%d+%H:%M:%S')
 	else 
 		git commit -m "Push without message"
 	fi	
@@ -16,7 +16,7 @@ function cdgpacp() {
 
 CURRENT_DIR=$(pwd)
 
-DATE="定時提交:"$(date +%Y%m%d)
+DATE="定時提交:"$(date '+%Y-%m-%d+%H:%M:%S')
 TIMESTAMP=$DATE
 
 WORKSPACE="/Users/linonon/Workspace"
@@ -29,12 +29,12 @@ Learn_Go_with_Github=$WORKSPACE/Learn-Go-with-Github
 MyMacConfig=$WORKSPACE/MyMacConfig
 leetcode_go_TDD=$WORKSPACE/leetcode-go-TDD
 
-cdgpacp $go_combat $TIMESTAMP
-cdgpacp $PATH_101 $TIMESTAMP
-cdgpacp $cloud_native_gocamp $TIMESTAMP
-cdgpacp $GO_Advanced_training_camp $TIMESTAMP
-cdgpacp $Learn_Go_with_Github $TIMESTAMP
-cdgpacp $MyMacConfig $TIMESTAMP
-cdgpacp $leetcode_go_TDD $TIMESTAMP
+cdgpacp $go_combat
+cdgpacp $PATH_101
+cdgpacp $cloud_native_gocamp
+cdgpacp $GO_Advanced_training_camp
+cdgpacp $Learn_Go_with_Github
+cdgpacp $MyMacConfig
+cdgpacp $leetcode_go_TDD
 
 cd $CURRENT_DIR
